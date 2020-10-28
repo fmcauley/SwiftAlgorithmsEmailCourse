86956112823de2ee70ac89e6c55333c224ffae70
Frank McAuley <frank.mcauley@gmail.com>
2020-10-28 08:09:49 -0400

    FM - Inital commit

---
+++ SwiftAlgorithmsEmailCourse/SwiftAlgorithmsEmailCourse.h
@@ -0,0 +1,18 @@
+//
+//  SwiftAlgorithmsEmailCourse.h
+//  SwiftAlgorithmsEmailCourse
+//
+//  Created by Frank McAuley on 10/28/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftAlgorithmsEmailCourse.
+FOUNDATION_EXPORT double SwiftAlgorithmsEmailCourseVersionNumber;
+
+//! Project version string for SwiftAlgorithmsEmailCourse.
+FOUNDATION_EXPORT const unsigned char SwiftAlgorithmsEmailCourseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftAlgorithmsEmailCourse/PublicHeader.h>
+
+
